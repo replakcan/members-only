4412bf8a151b5c562613634d4a253f40695aaaae
replakcan <alper.makcan@gmail.com>
2025-07-08 13:05:51 +0300

    Creates appjs with express and certain middlewares

---
+++ app.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('node:path')
+const session = require('express-session')
+const pool = require('./db/pool')
+const indexRouter = require('./routes/indexRouter')
+const passport = require('passport')
+const pgSession = require('connect-pg-simple')(session)
+
+const app = express()
+
+const sessionStore = new pgSession({ pool })
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+require('./config/passport')
+
+app.use(passport.session())
+
+app.use((req, res, next) => {
+  const { session, user } = req
+
+  console.log(session)
+  console.log(user)
+
+  next()
+})
+
+app.use('/', indexRouter)
+
+app.use((err, req, res, next) => {
+  console.log(err)
+
+  res.status(err.statusCode || 500).send(err.message)
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`express is running on port ${PORT}`)
+})
