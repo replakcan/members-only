bcb1842d14e59f8cc2d1adbcb71ab37f60d07e91
replakcan <alper.makcan@gmail.com>
2025-07-08 15:30:49 +0300

    Adds custom validators
    Does check if the passwords do match.
    Does check if the e-mail is already in use.

--- config/validator.js
+++ config/validator.js
@@ -1,4 +1,5 @@
 const { body, validationResult } = require('express-validator')
+const pool = require('../db/pool')
 
 const alphaErr = 'must contain only letters'
 const lengthErr = 'must be between 1 and 10 characters'
@@ -18,13 +19,30 @@ const validateUser = [
     .isLength({ min: 1, max: 10 })
     .withMessage(`Last name ${lengthErr}`),
 
-  body('email').notEmpty().withMessage('Email is required').isEmail().withMessage('Invalid email format'),
+  body('email')
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Invalid email format')
+    .custom(async (value) => {
+      const { rows } = await pool.query('SELECT * FROM users WHERE email=$1', [value])
+
+      const user = rows[0]
+
+      if (user) throw new Error('E-mail already in use')
+    }),
 
   body('password')
     .notEmpty()
     .withMessage('Password is required')
     .isLength({ min: 6 })
     .withMessage('Password must be at least 6 characters long'),
+
+  body('passwordConfirmation')
+    .custom((value, { req }) => {
+      return value === req.body.password
+    })
+    .withMessage('passwords do not match'),
 ]
 
 module.exports = { validateUser, validationResult }
