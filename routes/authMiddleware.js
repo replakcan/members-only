4153f5ed55d5321cfe39959614c31e250dfebc5c
replakcan <alper.makcan@gmail.com>
2025-07-08 13:06:25 +0300

    Creates simple auth-middlewares
    to check if the user is logged in and if the user is a member or not.

---
+++ routes/authMiddleware.js
@@ -0,0 +1,20 @@
+function isAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    next()
+  } else {
+    res.status(401).json({ msg: 'You are not authorized to view this resource' })
+  }
+}
+
+function isMember(req, res, next) {
+  if (req.isAuthenticated() && req.user.member) {
+    next()
+  } else {
+    res.status(401).json({ msg: 'You are not authorized to view this resource because you are not a member' })
+  }
+}
+
+module.exports = {
+  isAuth,
+  isMember,
+}
