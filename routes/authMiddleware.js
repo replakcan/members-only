a45a265db9b328b518907260da2b7ddb9b67e839
replakcan <alper.makcan@gmail.com>
2025-07-08 17:19:07 +0300

    Updates isMember middleware condition

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -7,7 +7,7 @@ function isAuth(req, res, next) {
 }
 
 function isMember(req, res, next) {
-  if (req.isAuthenticated() && req.user.member) {
+  if (req.isAuthenticated() && req.user.membership_status) {
     next()
   } else {
     res.status(401).json({ msg: 'You are not authorized to view this resource because you are not a member' })
